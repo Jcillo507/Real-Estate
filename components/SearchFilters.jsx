611522168c8b7d626510d344e8073ca16001eabb
Joe Cillo <JCillo507@yahoo.com>
2022-02-08 17:01:02 -0500

    search page done

---
+++ components/SearchFilters.jsx
@@ -0,0 +1,25 @@
+import { useEffect, useState } from "react";
+import {
+  Flex,
+  Select,
+  Box,
+  Text,
+  Inpout,
+  Spinner,
+  Icon,
+  Button,
+} from "@chakra-ui/react";
+import { useRouter } from "next/router";
+import { MdCancel } from "react-icons/md";
+import Image from "next/image";
+
+const SearchFilters = () => {
+  return (
+    <div>
+      filter
+      <div className=""></div>
+    </div>
+  );
+};
+
+export default SearchFilters;
